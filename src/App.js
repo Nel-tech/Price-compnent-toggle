9bc7043a5075b987436fa0967d4b8fe226e03f40
Lee145 <favournelson82@gmail.com>
2024-01-06 04:57:04 +0100

    src

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ export default function App() {
     <>
       <Section>
         <Header />
-        <Background/>
+
+        <PricingComponent />
+        <Background />
       </Section>
     </>
   );
@@ -24,7 +26,7 @@ function Background() {
 function Header() {
   return (
     <div>
-      <h1>Our Pricing</h1>
+      <h1 className="title">Our Pricing</h1>
 
       <div className="header-container">
         <h2>Annually</h2>
@@ -35,6 +37,53 @@ function Header() {
         </label>
         <h2>Monthly</h2>
       </div>
+    </div>
+  );
+}
+
+function PricingComponent() {
+  return (
+    <div>
+      <section>
+        <div className="componenet1">
+          <p>Basic</p>
+          <h1>$199.99</h1>
+          <line></line>
+          <p>500 GB Storage </p>
+          <line></line>
+          <p>2 Users Allowed </p>
+          <line></line>
+          <p>Send up to 3 GB </p>
+          <line></line>
+          <button>LEARN MORE</button>
+        </div>
+
+        <div className="componenet2">
+          <p>Professional</p>
+          <h1>$249.99</h1>
+          <line></line>
+          <p>1 TB Storage </p>
+          <line></line>
+          <p>5 Users Allowed </p>
+          <line></line>
+          <p>Send up to 10 GB </p>
+          <line></line>
+          <button>LEARN MORE</button>
+        </div>
+
+        <div className="componenet3">
+          <p>Master</p>
+          <h1>$399.99</h1>
+          <line></line>
+          <p>2 TB Storage </p>
+          <line></line>
+          <p>10 Users Allowed </p>
+          <line></line>
+          <p>Send up to 20 GB </p>
+          <line></line>
+          <button>LEARN MORE</button>
+        </div>
+      </section>
     </div>
   );
 }
