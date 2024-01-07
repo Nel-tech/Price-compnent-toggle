9ca05a079873734e1fbc499946841c7cbe96354e
Lee145 <favournelson82@gmail.com>
2024-01-07 10:53:47 +0100

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import { useState } from "react";
 export default function App() {
+  const [toggler, setToggler] = useState(false);
   return (
     <>
       <Section>
-        <Header />
-
-        <PricingComponent />
+        <Header toggler={toggler} setToggler={setToggler} />
         <Background />
+        <PricingComponent toggler={toggler} setToggler={setToggler} />
       </Section>
     </>
   );
@@ -23,7 +24,7 @@ function Background() {
 //   return <div className=" backgroundbottom"></div>;
 // }
 
-function Header() {
+function Header({ setToggler, toggler }) {
   return (
     <div>
       <h1 className="title">Our Pricing</h1>
@@ -32,7 +33,11 @@ function Header() {
         <h2>Annually</h2>
 
         <label className="switch">
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            checked={toggler}
+            onChange={() => setToggler(!toggler)}
+          />
           <span className="slider round"></span>
         </label>
         <h2>Monthly</h2>
@@ -41,46 +46,63 @@ function Header() {
   );
 }
 
-function PricingComponent() {
+function PricingComponent({ toggler }) {
+  const getPrice = (toggler) => {
+    if (toggler) {
+      return {
+        price1: "$19.99",
+        price2: "$24.99",
+        price3: "$39.99",
+      };
+    } else {
+      return {
+        price1: "$199.99",
+        price2: "$249.99",
+        price3: "$399.99",
+      };
+    }
+  };
+
+  const { price1, price2, price3 } = getPrice(toggler);
   return (
     <div>
-      <section>
+      <section className="pricing-container">
         <div className="componenet1">
-          <p>Basic</p>
-          <h1>$199.99</h1>
-          <line></line>
+          <h1>Basic</h1>
+          <h2>{price1}</h2>
+          <div className="line"></div>
           <p>500 GB Storage </p>
-          <line></line>
+          <div className="line"></div>
           <p>2 Users Allowed </p>
-          <line></line>
+          <div className="line"></div>
           <p>Send up to 3 GB </p>
-          <line></line>
+          <div className="line"></div>
           <button>LEARN MORE</button>
         </div>
 
         <div className="componenet2">
-          <p>Professional</p>
-          <h1>$249.99</h1>
-          <line></line>
+          <h1>Professional</h1>
+          <h2>{price2}</h2>
+          <div className="line"></div>
           <p>1 TB Storage </p>
-          <line></line>
+          <div className="line"></div>
           <p>5 Users Allowed </p>
-          <line></line>
+          <div className="line"></div>
           <p>Send up to 10 GB </p>
-          <line></line>
+          <div className="line"></div>
           <button>LEARN MORE</button>
         </div>
 
         <div className="componenet3">
-          <p>Master</p>
-          <h1>$399.99</h1>
-          <line></line>
+          <h1>Master</h1>
+          <h2>{price3}</h2>
+          <div className="line"></div>
           <p>2 TB Storage </p>
-          <line></line>
+          <div className="line"></div>
           <p>10 Users Allowed </p>
-          <line></line>
+          <div className="line"></div>
           <p>Send up to 20 GB </p>
-          <line></line>
+          <div className="line"></div>
           <button>LEARN MORE</button>
         </div>
       </section>
